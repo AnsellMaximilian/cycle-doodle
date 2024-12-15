264ed41c3f311e630871c7c607a3468a261e5f92
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-12-15 07:35:07 +0700

    You belong to this team

--- app/play/UserInfo.tsx
+++ app/play/UserInfo.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { TEAM_IMAGES } from "@/const";
 import { usePlay } from "@/contexts/PlayContext";
-import { TeamRoleKeys } from "@/types";
+import { TeamKeys, TeamRoleKeys } from "@/types";
 import clsx from "clsx";
+import Image from "next/image";
 import React from "react";
 
 const getRoleDesc = (role: TeamRoleKeys) => {
@@ -25,11 +27,21 @@ export default function UserInfo() {
 
   return (
     <div className={clsx("gap-8 grid grid-cols-2 ")}>
-      <div className="border border-gray-200 shadow-sm rounded-md p-4">
-        <div className=" font-bold text-xl uppercase">{userTeam.name}</div>
-        <p className="text-sm text-left">{userTeam.description}</p>
+      <div className="border border-gray-200 shadow-sm rounded-md p-4 py-8">
+        <div className="flex items-center gap-8">
+          <Image
+            src={TEAM_IMAGES[userTeam.key as TeamKeys]}
+            width={80}
+            height={80}
+            alt="Team Logo"
+          />
+          <div>
+            <div className=" font-bold text-xl uppercase">{userTeam.name}</div>
+            <p className="text-sm text-left">{userTeam.description}</p>
+          </div>
+        </div>
       </div>
-      <div className="border border-gray-200 shadow-sm rounded-md p-4">
+      <div className="border border-gray-200 shadow-sm rounded-md p-4 py-8">
         <div className=" font-bold text-xl uppercase">{teamRole}</div>
         <p className="text-sm text-left">{getRoleDesc(teamRole)}</p>
       </div>
